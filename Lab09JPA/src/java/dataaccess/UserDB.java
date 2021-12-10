43043f64e97645394cb2034d20f681a119c2a258
849533 <849533@SAIT227692IT.ACDM.DS.SAIT.CA>
2021-12-09 18:04:48 -0700

    UserDB.java -changed to JPA

---
+++ Lab09JPA/src/java/dataaccess/UserDB.java
@@ -0,0 +1,73 @@
+package dataaccess;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import models.User;
+
+public class UserDB {
+    
+    public List<User> getAll() throws Exception {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        try {
+            List<User> users = em.createNamedQuery("User.findAll", User.class).getResultList();
+            return users;
+        } finally {
+            em.close();
+        }
+    }
+    
+    public User get(String email) throws Exception {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        try {
+            User user = em.find(User.class, email);
+            return users;
+        } finally {
+            em.close();
+        }
+    }
+    
+    public void insert(User user) throws Exception {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction et = em.getTransaction();
+        try {
+            et.begin();
+            em.persist(user);
+            em.merge(user);
+            et.commit();
+        } catch (Exception e) {
+            et.rollback();
+        }finally {
+            em.close();
+        }
+    }
+    
+    public void update(User user) throws Exception {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction et = em.getTransaction();
+        try {
+            et.begin();
+            em.merge(user);
+            et.commit();
+        } catch (Exception e) {
+            et.rollback();
+        }finally {
+            em.close();
+        }
+    }
+    
+    public void delete(User user) throws Exception {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction et = em.getTransaction();
+        try {
+            et.begin();
+            em.remove(em.merge(user));
+            em.merge(user);
+            et.commit();
+        } catch (Exception e) {
+            et.rollback();
+        }finally {
+            em.close();
+        }
+    }
+}
